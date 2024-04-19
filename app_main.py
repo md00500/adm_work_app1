79f9d08a12bb08252401b6432ffa78c75adf0862
md00500 <yi--pp--ee@outlook.jp>
2024-04-19 09:12:48 +0900

    変更内容

--- app_main.py
+++ app_main.py
@@ -57,5 +57,5 @@ def upload_file():
 
 if __name__ == "__main__":
 #    app.run()
-    port = int(os.environ.get('PORT', 8080))
+    port = int(os.environ.get('PORT', 8081))
     app.run(host ='0.0.0.0',port = port)
